3e4ab29c2c1fc754edc743f7d6d0f3d916f5432e
pwagner <pwagner>
2023-12-24 11:52:48 +0100

    Enable cross compilation
    The target architecture is arm but we still need a x86 build for
    the end to end tests which we will implement soon.

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo -e "\e[32m-------------------- Compile and run ecm --------------------\e[0m"
-RUSTFLAGS="-D warnings" cargo build
---
+++ run_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -e "\e[32m-------------------- Compile ecm --------------------\e[0m"
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+user_input=${1:-input_missing}
+
+if [ "$user_input"  == "x86" ]; then
+    echo -e "\e[32mBuild for target architecture $user_input ... \e[0m"
+    RUSTFLAGS="-D warnings" cargo build
+
+elif [ "$user_input"  == "arm" ]; then
+    echo -e "\e[32mBuild for target architecture $user_input ... \e[0m"
+    RUSTFLAGS="-D warnings" cargo build
+
+elif [ "$user_input"  == "clean" ]; then
+    echo -e "\e[32mClean\e[0m"
+    cargo clean
+else
+    echo -e "\e[31mMissing user input\e[0m"
+    echo -e "\e[31mUsage ${0} <x86/arm/clean>\e[0m"
+fi
